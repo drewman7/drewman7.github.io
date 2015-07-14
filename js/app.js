a51a5ea6b2f00fc72b28bc4c5c1f36b6688cd768
Drew <drewstuff5@hotmail.com>
2015-07-13 20:20:26 -0400

    test

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ var viewModel = function() {
     var CLIENT_ID = '843769279902-c5r8m7millp03vnvnfufnoinooat96lv.apps.googleusercontent.com';
 
     var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-
+    console.log('in function');
     /**
      * Check if current user has authorized this application.
      */
