a3149e504caffdfc340e9f540a484895de8135a7
Drew <drewstuff5@hotmail.com>
2015-09-29 12:20:03 -0400

    testcount

--- js/app.js
+++ js/app.js
@@ -71,7 +71,7 @@ var timer = function() {
 	if (modelData.count <= 0){
 		//clearInterval(counter);
 		// console.log(viewModel.pageTitle());
-		modelData.pageTitle("test" + octopus.testCount++);
+		modelData.pageTitle("test" + octopus.init.testCount++);
 		modelData.count = 10;
 		return;
 	}
