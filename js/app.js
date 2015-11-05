3596964fd686fd1193303ecc4764612e1eec1240
Drew <drewstuff5@hotmail.com>
2015-11-04 21:59:04 -0500

    tesat

--- js/app.js
+++ js/app.js
@@ -115,8 +115,8 @@ var octopus = {
 			  modelData.title = new Date();
 			  //console.log(modelData.daysOfMonth[eventDate.getMonth()]);
 			  //console.log(modelData.daysOfMonth[modelData.title.getMonth()]); 
-			  console.log(whenDay);
-			  console.log(modelData.title.getDate() + 1); 
+			  console.log(whenDay + 1);
+			  console.log(modelData.title.getDate() + 2); 
 			  
 	//		  If (modelData.daysOfMonth[eventDate.getMonth()] === modelData.daysOfMonth[modelData.title.getMonth()]) {
 					if (modelData.title.getDate() + 1 === whenDay) {
